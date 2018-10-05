96d54d52211ff63a7e555177568e1bd139ab6b81
Laura Ibrahim <lauraibrahim02@gmail.com>
2018-10-04 20:18:18 -0400

    new changes

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Big Mac Burger', false);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Jalapeno King Burger', false);
+INSERT INTO burgers (burger_name, Devoured) VALUES ('Baconator Burger', false);
+
